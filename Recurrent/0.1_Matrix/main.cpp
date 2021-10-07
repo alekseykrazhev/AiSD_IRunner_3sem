c2c467391289900d425f129a06ef668afd62b1b0
Alex <78850311+alekseykrazhev@users.noreply.github.com>
2021-10-07 13:58:27 +0300

    a

--- Recurrent/0.1_Matrix/main.cpp
+++ Recurrent/0.1_Matrix/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 int main() {
+    std::ifstream in ("input.txt");
+    std::ofstream out ("output.txt");
+
+    if (!in.is_open()){
+        std::cerr << "Error opening file" ;
+        return -1;
+    }
+
+    int s, a, b;
+    std::vector <int> vec;
+    in >> s;
+    for (int i = 0; i < s; ++i) {
+        in >> a >> b;
+        vec.push_back(a);
+        vec.push_back(b);
+    }
+    std::vector<std::vector<int>> matrix;
+
 
     return 0;
 }
